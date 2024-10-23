8e1a3cac17809990718589d11876a48e1016b421
jaanli <jaan.li@jaan.li>
2024-10-23 12:12:17 -0400

    add discharge note dbt models

---
+++ data_processing/models/note/radiology.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized = 'external',
+    location = '~/data/physionet.org/processed/mimiciv/note/{{ this.name }}.parquet'
+)}}
+
+WITH source_data AS (
+    SELECT * FROM read_csv_auto('~/data/physionet.org/files/mimiciv/note/radiology.csv.gz',
+                                header=true,
+                                filename=true,
+                                compression='gzip')
+)
+
+SELECT
+    CAST(note_id AS VARCHAR) AS note_id,
+    CAST(subject_id AS INTEGER) AS subject_id,
+    CAST(hadm_id AS INTEGER) AS hadm_id,
+    CAST(note_type AS VARCHAR) AS note_type,
+    CAST(note_seq AS INTEGER) AS note_seq,
+    CAST(charttime AS TIMESTAMP) AS charttime,
+    CAST(storetime AS TIMESTAMP) AS storetime,
+    CAST(text AS VARCHAR) AS text
+
+FROM source_data
---
+++ data_processing/models/note/radiology_detail.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized = 'external',
+    location = '~/data/physionet.org/processed/mimiciv/note/{{ this.name }}.parquet'
+)}}
+
+WITH source_data AS (
+    SELECT * FROM read_csv_auto('~/data/physionet.org/files/mimiciv/note/radiology_detail.csv.gz',
+                                header=true,
+                                filename=true,
+                                compression='gzip')
+)
+
+SELECT
+    CAST(note_id AS VARCHAR) AS note_id,
+    CAST(subject_id AS INTEGER) AS subject_id,
+    CAST(field_name AS VARCHAR) AS field_name,
+    CAST(field_value AS VARCHAR) AS field_value,
+    CAST(field_ordinal AS INTEGER) AS field_ordinal
+
+FROM source_data
